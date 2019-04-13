63fd7a315d6af5535d4873e00e0d007e90af5d09
jdreina7 <jdreinar@hotmail.com>
2019-04-13 00:20:50 -0500

    Segundo commit: Conectando con mlab

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if (GLOBAL_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/bocon_db'
 } else {
-    urlDB = 'mongodb://super_bocon_db:qazxsw21@2019@ds139576.mlab.com:39576/bocon_db'
+    urlDB = 'mongodb://super-admin:qazxsw21@ds139576.mlab.com:39576/bocon_db'
 }
 
 ENV_DB = urlDB;
